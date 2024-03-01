a9571928d6ea54b7a2d6a93a86299a5d4cdc7f3e
Thomas Ziereis <thomas.ziereis@web.de>
2024-03-01 23:14:41 +0100

    more

--- main.zig
+++ main.zig
@@ -37,9 +37,8 @@ const BinaryReader = struct {
         if (self.pos + @sizeOf(T) > self.data.len) {
             return ReaderError.outOfMemoryError;
         }
-        const value: T = @bitCast(std.mem.readInt(T, self.data[self.pos..][0..@sizeOf(T)], .little));
-        self.pos += @sizeOf(T);
-        return value;
+        defer self.pos += @sizeOf(T);
+        return @bitCast(std.mem.readInt(T, self.data[self.pos..][0..@sizeOf(T)], .little));
     }
 
     fn readIntLeb(self: *Self, comptime T: type) ReaderError!T {
@@ -57,6 +56,15 @@ const BinaryReader = struct {
             }
         }
         return @as(T, @intCast(result));
+    }
+
+    fn readStr(self: *Self) ReaderError![]const u8 {
+        const len = try self.readIntLeb(u32);
+        if (self.pos + len > self.data.len) {
+            return ReaderError.outOfMemoryError;
+        }
+        defer self.pos += len;
+        return self.data[self.pos .. self.pos + len];
     }
 
     fn hasMore(self: *Self) bool {
@@ -116,44 +124,144 @@ const WasmFunctionType = struct {
         var first = true;
         for (self.argTypes) |*arg| {
             if (!first) {
-                print(", ");
+                print(", ", .{});
             }
-            print("{}", arg.str());
+            print("{}", .{arg});
             first = false;
         }
 
         // Print return type
-        print(") -> {}", self.retType.str());
+        print(") -> {}", .{self.retType});
     }
 };
 
-const WasmTypeSection = struct {};
+const WasmTypeSection = struct {
+    prototypes: []WasmFunctionType,
+    const Self = @This();
+
+    pub fn parseSection(self: *Self, alloc: std.mem.Allocator, reader: *BinaryReader) !void {
+        const count = try reader.readIntLeb(u32);
+        self.prototypes = try alloc.alloc(WasmFunctionType, count);
+        for (self.prototypes) |*proto| {
+            try proto.parse(alloc, reader);
+        }
+    }
+
+    pub fn dump(self: *Self) void {
+        for (self.prototypes) |*proto| {
+            proto.dump();
+        }
+    }
+};
+
+const WasmFunctionSection = struct {
+    functions: []u32,
+    const Self = @This();
+
+    pub fn parseSection(self: *Self, alloc: std.mem.Allocator, reader: *BinaryReader) !void {
+        const count = try reader.readIntLeb(u32);
+        self.functions = try alloc.alloc(u32, count);
+        for (self.functions) |*func| {
+            func.* = try reader.readIntLeb(u32);
+        }
+    }
+
+    pub fn dump(self: *Self) void {
+        for (self.functions) |idx| {
+            print("Function Index: {}\n", .{idx});
+        }
+    }
+};
+
+const ExportType = enum(u8) {
+    function,
+    table,
+    memory,
+    global,
+};
+
+const ExportEntity = struct {
+    name: []u8,
+    idx: u32,
+    exportType: ExportType,
+
+    pub fn dump(self: *ExportEntity) void {
+        print("ExportEntity: {s} -> {}, {}\n", .{ self.name, self.exportType, self.idx });
+    }
+};
+
+const WasmExportSection = struct {
+    exports: []ExportEntity,
+    const Self = @This();
+
+    pub fn parseSection(self: *Self, alloc: std.mem.Allocator, reader: *BinaryReader) !void {
+        const count = try reader.readIntLeb(u32);
+        self.exports = try alloc.alloc(ExportEntity, count);
+
+        for (self.exports) |*exp| {
+            const strRef = try reader.readStr();
+            exp.* = ExportEntity{
+                .name = try alloc.alloc(u8, strRef.len),
+                .exportType = @enumFromInt(try reader.read(u8)),
+                .idx = try reader.readIntLeb(u32),
+            };
+            std.mem.copyForwards(u8, exp.name, strRef);
+        }
+    }
+
+    pub fn dump(self: *Self) void {
+        for (self.exports) |*exp| {
+            exp.dump();
+        }
+    }
+};
+
+const WasmIR = struct {};
+
+const CodeGen = struct {
+    pub fn genIR() !WasmIR {}
+};
 
 const WasmModule = struct {
     allocator: std.mem.Allocator,
-    typeSection: WasmFunctionType = undefined,
+    typeSection: WasmTypeSection = undefined,
+    functionSection: WasmFunctionSection = undefined,
+    exportSection: WasmExportSection = undefined,
+
     const Self = @This();
 
     pub fn run(self: *Self, wasmCode: []const u8) !void {
         var reader = BinaryReader{ .data = wasmCode };
+        print("{x}\n", .{reader.data});
         const magic = try reader.read(u32);
         try wasmValidateEq(magic, 0x6d736100);
         const version = try reader.read(u32);
         try wasmValidateEq(version, 1);
         while (reader.hasMore()) {
             const sectionId: WasmSectionId = @enumFromInt(try reader.read(u8));
-            try wasmValidateLt(@intFromEnum(sectionId), @intFromEnum(WasmSectionId.EnumSize));
+            const sectionsize = try reader.readIntLeb(u32);
+            print("Section size: {}\n", .{sectionsize});
+            try wasmValidateLt(
+                @intFromEnum(sectionId),
+                @intFromEnum(WasmSectionId.EnumSize),
+            );
             print("{}\n", .{sectionId});
 
             switch (sectionId) {
                 WasmSectionId.typeSection => {
-                    try self.typeSection.parse(self.allocator, &reader);
+                    try self.typeSection.parseSection(self.allocator, &reader);
                     self.typeSection.dump();
+                },
+                WasmSectionId.functionSection => {
+                    try self.functionSection.parseSection(self.allocator, &reader);
+                    self.functionSection.dump();
+                },
+                WasmSectionId.exportSection => {
+                    try self.exportSection.parseSection(self.allocator, &reader);
+                    self.exportSection.dump();
                 },
                 else => break,
             }
-
-            break;
         }
     }
 };
